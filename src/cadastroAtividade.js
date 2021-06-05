497ca30602e895e1c2a68da9882da71912003b3c
letdesa <ldsacs@gmail.com>
2021-06-05 00:51:17 -0300

    Foi Adicionado cadastro de atividades

---
+++ src/cadastroAtividade.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	// Ativa balão de dica
+	$('[data-toggle="tooltip"]').tooltip();
+	
+	// Seleciona/Deseleciona as caixas
+	var checkbox = $('table tbody input[type="checkbox"]');
+	$("#selectAll").click(function(){
+		if(this.checked){
+			checkbox.each(function(){
+				this.checked = true;                        
+			});
+		} else{
+			checkbox.each(function(){
+				this.checked = false;                        
+			});
+		} 
+	});
+	checkbox.click(function(){
+		if(!this.checked){
+			$("#selectAll").prop("checked", false);
+		}
+	});
+});
